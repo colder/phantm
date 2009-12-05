c9add7c554b71d56af07db91144eeca78edbd89c
Etienne Kneuss <colder@php.net>
2009-12-05 02:17:16 +0100

    add internal function stuff

---
+++ src/phpanalysis/controlflow/InternalFunctions.scala
@@ -0,0 +1,11 @@
+package phpanalysis.controlflow
+
+import Types._
+import parser.Trees._
+import analyzer.Symbols._
+
+object InternalFunctions {
+    def lookup(id: Identifier): Option[FunctionSymbol] = {
+        None
+    }
+}
