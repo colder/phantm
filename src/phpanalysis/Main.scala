5d69cb8393176ad21cbbc84b7e2113db3d5729d3
Etienne Kneuss <colder@php.net>
2009-12-15 04:06:16 +0100

    Introduce the --includepath option

--- src/phpanalysis/Main.scala
+++ src/phpanalysis/Main.scala
@@ -31,6 +31,9 @@ object Main {
     def handleArgs(args: List[String]): Unit = args match {
         case "--symbols" :: xs => displaySymbols = true; handleArgs(xs);
         case "--debug" :: xs => displayDebug = true; handleArgs(xs);
+        case "--includepath" :: ip :: xs =>
+            includePaths = ip.split(":").toList
+            handleArgs(xs)
         case "--progress" :: xs => displayProgress = true; handleArgs(xs);
         case x :: xs => files = files ::: x :: Nil; handleArgs(xs);
         case Nil => 
