df8e21dd88629b7b5553d1e28d80a8e5445f446c
Etienne Kneuss <colder@php.net>
2010-04-20 01:45:29 +0200

    phpanalysis -> phantm

--- helpers.sh
+++ helpers.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scala -classpath classes:lib/cup/dist/java-cup-11a-runtime.jar phpanalysis.helpers.Main $*
+scala -classpath classes:lib/cup/dist/java-cup-11a-runtime.jar phantm.helpers.Main $*
