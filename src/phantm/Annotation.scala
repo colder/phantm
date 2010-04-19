df0abf7e80b3bfabc4d25cc2ee6c36cd939ece5f
Etienne Kneuss <colder@php.net>
2010-04-19 20:03:05 +0200

    First step towards annotations refactoring

---
+++ src/phantm/Annotation.scala
@@ -0,0 +1,33 @@
+package phantm
+
+sealed trait Annotation
+
+trait TypeAnnotation extends Annotation {
+    import analyzer.Types.{Type, TAny}
+
+    var typ: Type = TAny
+
+    def annotateFromT(from: TypeAnnotation): this.type = { typ = from.typ; this }
+}
+
+trait FunctionTypeAnnotation extends Annotation {
+    import analyzer.Types.{FunctionType, TFunctionAny}
+
+    var ftyps = Set[FunctionType]() + TFunctionAny
+
+    def registerFType(ftyp: FunctionType): this.type = { ftyps += ftyp; this }
+
+    def annotateFromFT(from: FunctionTypeAnnotation): this.type = { ftyps = from.ftyps; this }
+}
+
+trait CommentAnnotation  extends Annotation {
+    var comment: Option[String] = None
+
+    def attachComment(com: Option[String]): this.type = {
+        comment = com
+        this
+    }
+
+    def annotateFromC(from: CommentAnnotation): this.type = { comment = from.comment; this }
+}
+
