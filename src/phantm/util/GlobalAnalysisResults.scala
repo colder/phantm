77db73e504faf93ff4128b31ca02976fe09648ff
Etienne Kneuss <colder@php.net>
2010-06-15 09:55:23 +0200

    Add file

---
+++ src/phantm/util/GlobalAnalysisResults.scala
@@ -0,0 +1,12 @@
+package phantm.util
+
+import phantm.types.{Type, TypeEnvironment, ObjectStore}
+import phantm.symbols.FunctionSymbol
+
+class GlobalAnalysisResults {
+    var inlineCache: Map[FunctionSymbol, Map[List[Type], (Type, ObjectStore)]] = Map().withDefaultValue(Map())
+    var globalCalls: Map[FunctionSymbol, Map[String, TypeEnvironment]] = Map().withDefaultValue(Map())
+    var endGlobals: Option[TypeEnvironment] = None
+    var reachableFromMain: Map[FunctionSymbol, Set[FunctionSymbol]] = Map().withDefaultValue(Set())
+    var summary: Map[FunctionSymbol, Int] = Map()
+}
